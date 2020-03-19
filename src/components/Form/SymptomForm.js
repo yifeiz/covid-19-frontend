4a8cd1f9629568be05ec0c74bb535e6c8d2c2f66
Owen <obrake3@gmail.com>
2020-03-19 14:08:09 -0400

    Refactored custom styles to new file

--- src/components/Form/SymptomForm.js
+++ src/components/Form/SymptomForm.js
@@ -11,34 +11,9 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import Box from "@material-ui/core/Box";
-import { TextField } from "@material-ui/core";
-import {makeStyles,fade} from '@material-ui/core/styles'
+import PostalTextField from './FormCustomStyles'
 var Recaptcha = require("react-recaptcha");
 
-const postalStyle = makeStyles(theme => ({
-  root: {
-    border: '4px solid #e2e2e1',
-    overflow: 'hidden',
-    borderRadius: 5,
-    backgroundColor: '#fcfcfb',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    '&:hover': {
-      backgroundColor: '#fff',
-    },
-    '&$focused': {
-      backgroundColor: '#fff',
-      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-      borderColor: theme.palette.primary.main,
-    },
-  },
-  focused: {},
-}));
-
-function PostalTextField(props) {
-  const classes = postalStyle();
-
-  return <TextField InputProps={{ classes, disableUnderline: true }} {...props} />;
-}
 
 class SymptomForm extends React.Component {
   state = { isVerified: false };
