3e9679d841f154249043cb1e459cd9bd701f8f6f
Samuel Hao <samuelhanhao@gmail.com>
2020-03-20 12:20:41 -0400

    Info updates

--- src/components/info/Paragraphs/Para2.jsx
+++ src/components/info/Paragraphs/Para2.jsx
@@ -8,20 +8,24 @@ const Para2 = () => (
       Symptoms for COVID-19 are similar to those for influenza or other
       respiratory illnesses. The most common symptoms include: fever, cough, and
       shortness of breath. Less common symptoms can include extreme tiredness,
-      sore throat, and runny nose.
+      sore throat, and runny nose
     </p>
     <p>
       Most people (about 80%) recover from this disease without needing special
       treatment, but it can also cause serious illness. Those who are older and
       those with other medical problems are more likely to develop serious
       illness, which can include difficulty breathing and pneumonia; there is
-      also a risk of death in severe cases.
+      also a risk of death in severe cases
     </p>
     <p>
       While we are still learning about how COVID-19 affects people, older
       persons and persons with pre-existing medical conditions (such as high
       blood pressure, heart disease, lung disease, active cancer, dialysis, or
-      diabetes) appear to develop serious illness more often than others.
+      diabetes) appear to develop serious illness more often than others
+    </p>
+    <p>
+      Source: Public Health Ontario
+      (https://www.ontario.ca/page/2019-novel-coronavirus)
     </p>
   </React.Fragment>
 );
