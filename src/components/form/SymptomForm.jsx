611f534eca350c99ada7d5796c1f87efc91e1f13
Eric Zhai <e.b.zhai@gmail.com>
2020-03-21 16:32:51 -0400

    extract symptoms constants

--- src/components/form/SymptomForm.jsx
+++ src/components/form/SymptomForm.jsx
@@ -4,21 +4,17 @@ import { Field, reduxForm } from "redux-form";
 import { Table, Row, Col, Container } from "reactstrap";
 
 import { validate } from "./SymptomFormUtils";
+import {
+  FORM_HEADER,
+  FORM_DISCLAIMER,
+  FORM_QUESTIONS,
+  FORM_POSTAL_CODE_QUESTION
+} from "./SymptomFormConstants";
 import image from "../../assets/science.png";
 import "./SymptomForm.css";
 import Box from "@material-ui/core/Box";
 import { PostalTextField, RadioButton } from "./SymptomFormFields";
 var Recaptcha = require("react-recaptcha");
-
-const QUESTIONS = [
-  "Do you have a fever, chills or shakes?",
-  "Do you have a new or worsening cough?",
-  "Are you experiencing shortness of breath (difficulty breathing, breathlessness)?",
-  "Are you 60 years of age or older?",
-  "Do you have any of the following medical conditions: diabetes, heart disease, active cancer, history of stroke, asthma, COPD, dialysis, or are immunocompromised?",
-  "Have you traveled outside of Canada within the last 14 days?",
-  "Have you had close contact with someone who is coughing, has a fever, or is otherwise sick and has been outside of Canada in the last 14 days?"
-];
 
 const SymptomForm = props => {
   const [isVerified, setIsVerified] = useState(false);
@@ -70,17 +66,8 @@ const SymptomForm = props => {
     <Container fluid>
       <Row>
         <Col md="10" >
-          <h4 className="header">Tell us how you feel!</h4>
-          <p className="formDisclaimer">
-            The following questionnaire is designed to help assess your risk
-            factors for COVID-19 infection and to provide guidance on how to
-            keep yourself, your family, and your community healthy. The
-            questions are based on the best available guidance from Canadian
-            public health agencies and other stakeholders, and will be
-            updated regularly. Your answers are collected anonymously, and
-            will be aggregated to help healthcare providers gauge the spread
-            of COVID-19.
-          </p>
+          <h4 className="header">{FORM_HEADER}</h4>
+          <p className="form-disclaimer">{FORM_DISCLAIMER}</p>
         </Col>
         <Col md="2" className="frame">
           <img className="microscope-icon" src={image} alt="Microscope Icon" />
@@ -101,12 +88,9 @@ const SymptomForm = props => {
                 </tr>
               </thead>
               <tbody>
-                {renderQuestions(QUESTIONS)}
+                {renderQuestions(FORM_QUESTIONS)}
                 <tr key="postal-code">
-                  <p className="question">
-                    What are the first three characters of the postal code of
-                    your current residence?
-                  </p>
+                  <p className="question">{FORM_POSTAL_CODE_QUESTION}</p>
                   <td colSpan="2">
                     <Field name="postalCode" component={PostalTextField}/>
                   </td>
