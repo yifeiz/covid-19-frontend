fe54d782497ea260454713907b2448ba25aa1ad1
Samuel Hao <samuelhanhao@gmail.com>
2020-03-20 23:55:18 -0400

    Box shadow

--- src/components/about-us/Profile.jsx
+++ src/components/about-us/Profile.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 import "./Profile.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Profile = props => {
   return (
     <div className="card text-center">
       <div className="overflow">
-        <img
-          src={require(`${props.src}`)}
-          alt={props.name}
-          className="card-img-top"
-        />
+        <a href="https://www.linkedin.com/in/yifei-zhang1/">
+          <img
+            src={require(`${props.src}`)}
+            alt={props.name}
+            className="card-img-top"
+          />
+        </a>
       </div>
       <div className="card-body text-dark">
         <p className="card-text">{props.name}</p>
-        <a
-          href="https://www.linkedin.com/in/yifei-zhang1/"
-          target="_blank"
-          classname="link"
-        ></a>
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
       </div>
     </div>
   );
