4f00b275db4d8441eb9bcc33a7d1e0bf8e5d5da5
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-27 21:28:28 -0400

    spacing

--- src/components/disclaimer/Disclaimer.js
+++ src/components/disclaimer/Disclaimer.js
@@ -4,14 +4,13 @@ import "./Disclaimer.css";
 
 const Disclaimer = () => (
   <div className="Disclaimer">
-    <div>
-      We are a group of scientists, engineers and clinicians who have created an
-      online screening tool to provide information on COVID-19. This app is
-      designed to help increase awareness and flatten the curve of the spread of
-      COVID-19 by informing Canadians and healthcare systems of relevant
-      information. Should you have any concerns, please review our
-    </div>
+    We are a group of scientists, engineers and clinicians who have created an
+    online screening tool to provide information on COVID-19. This app is
+    designed to help increase awareness and flatten the curve of the spread of
+    COVID-19 by informing Canadians and healthcare systems of relevant
+    information. Should you have any concerns, please review our
     <a href="https://drive.google.com/open?id=1dbkrOLBBp_yqus-oeQ_JXdanDbdb8u-6">
+      {" "}
       Privacy Policy{" "}
     </a>
     and
