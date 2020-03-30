59af1f299a35fc0a835c1230b98b3293c84af8b4
Samuel Hao <samuelhanhao@gmail.com>
2020-03-29 23:12:25 -0700

    Added notLoggedIn component

---
+++ src/components/notLoggedIn/notLoggedIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./notLoggedIn.css";
+
+const notLoggedIn = () => (
+  <React.Fragment>
+    <h4 className="header">
+      Please log in to view this page and log your health
+    </h4>
+  </React.Fragment>
+);
+
+export default notLoggedIn;
