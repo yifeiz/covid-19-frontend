1ba580c9918d85e53de5eb30e16e48c3984fde14
Owen <obrake3@gmail.com>
2020-04-01 11:56:35 -0400

    Remove actions/index.js db.staging

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import db from "../apis/db.staging";
+import db from "../apis/db";
 
 export const submitForm = formValues => async dispatch => {
   const response = await db.post("/submit", formValues);
