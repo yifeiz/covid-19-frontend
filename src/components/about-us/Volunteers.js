478557c02c36f986462084af4e87b4b16874f85a
Yifei Zhang <yifei7.zhang@gmail.com>
2020-04-06 10:32:24 -0400

    hotfix

--- src/components/about-us/Volunteers.js
+++ src/components/about-us/Volunteers.js
@@ -3,31 +3,31 @@ const volunteers = [
     name: "Shrey Jain",
     src: "./Headshots/shrey.png",
     link: "https://www.linkedin.com/in/shrey-j-9869b213a/",
-    role: "Président"
+    role: "Président",
   },
   {
     name: "Yifei Zhang",
     src: "./Headshots/yifei.png",
     link: "https://www.linkedin.com/in/yifei-zhang1/",
-    role: "Chef d'équipe dev et DevOps"
+    role: "Chef d'équipe dev et DevOps",
   },
   {
-    name: "Martin Staadacker",
+    name: "Martin Staadecker",
     src: "./Headshots/martin.png",
     link: "https://www.linkedin.com/in/staad/",
-    role: "Chef d'équipe de l'intégration Carte et DevOps"
+    role: "Chef d'équipe de l'intégration Carte et DevOps",
   },
   {
     name: "Arthur Allshire",
     src: "./Headshots/arthur.png",
     link: "https://www.linkedin.com/in/arthur-allshire-56b06a16a/",
-    role: "Chef d'équipe Cloud et DevOps"
+    role: "Chef d'équipe Cloud et DevOps",
   },
   {
     name: "Surya Krishnan",
     src: "./Headshots/surya.png",
     link: "https://www.linkedin.com/in/suryakrsh/",
-    role: "Chef d'équipe de Carte frontale et DevOps"
+    role: "Chef d'équipe de Carte frontale et DevOps",
   },
   {
     name: "Shafi U. Bhuiyan",
@@ -35,14 +35,14 @@ const volunteers = [
     role: "Public Health Advisor",
     degrees: ", MBBS, MPH, MBA, PhD",
     titles:
-      "Directeur, Conseil d'administration, Canadian Coalition for Global Health Research"
+      "Directeur, Conseil d'administration, Canadian Coalition for Global Health Research",
   },
   {
     name: "Geoffrey Hinton",
     src: "./Headshots/geoffrey.png",
     role: "Advisor",
     degrees: ", PhD",
-    titles: "Conseiller scientifique en chef de Vector Institute"
+    titles: "Conseiller scientifique en chef de Vector Institute",
   },
   {
     name: "Minto Jain",
@@ -50,7 +50,7 @@ const volunteers = [
     link: "https://www.linkedin.com/in/minto-jain-19380333/",
     role: "Advisor",
     degrees: ", MHPE, MD, FRSCS",
-    titles: "Intensiviste"
+    titles: "Intensiviste",
   },
   {
     name: "Marzyeh Ghassemi",
@@ -58,7 +58,7 @@ const volunteers = [
     link: "https://www.linkedin.com/in/marzyehghassemi",
     role: "Advisor and Director",
     degrees: ", PhD",
-    titles: "Directeur de CIFAR AI et Directeur de Recherche Canada"
+    titles: "Directeur de CIFAR AI et Directeur de Recherche Canada",
   },
 
   {
@@ -67,7 +67,7 @@ const volunteers = [
     link: "https://www.linkedin.com/in/drabymathews",
     role: "Advisor",
     degrees: "",
-    titles: "Healthcare IT Specialist"
+    titles: "Healthcare IT Specialist",
   },
   {
     name: "Nick Frosst",
@@ -75,110 +75,117 @@ const volunteers = [
     link: "https://www.linkedin.com/in/nick-frosst-19b80463/",
     role: "Advisor and Director",
     degrees: "",
-    titles: "Fondateur de Cohere"
+    titles: "Fondateur de Cohere",
+  },
+  {
+    name: "Ranjani Somayaji",
+    src: "./Headshots/ranjani.png",
+    role: "Advisor",
+    degrees: ", MD, MPH, FRCPC",
+    titles: "Spécialiste des maladies infectieuses et épidémiologiste",
   },
   {
     name: "Anastasia Razdaibiedina",
     src: "./Headshots/anastasia.png",
     link: "https://www.linkedin.com/in/anastasia-razdaibiedina-438929197/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Emily Tao",
     src: "./Headshots/emily.png",
     link: "https://www.linkedin.com/in/emilytao2000/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Samuel Hao",
     src: "./Headshots/samuel.png",
     link: "https://www.linkedin.com/in/samuelhao/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Charlie Chen",
     src: "./Headshots/charlie.png",
     link: "https://www.linkedin.com/in/hyu-chen/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "William Wen",
     src: "./Headshots/william.png",
     link: "https://www.linkedin.com/in/william-wen360/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Xavier Hébert-Couturier",
     src: "./Headshots/xavier.png",
     link: "https://www.linkedin.com/in/xavierhc/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Steven Van",
     src: "./Headshots/steven.png",
     link: "https://www.linkedin.com/in/steven-van",
-    role: "Chef d'équipe Média"
+    role: "Chef d'équipe Média",
   },
   {
     name: "Sejal Jain",
     src: "./Headshots/sejal.png",
     link: "https://www.linkedin.com/in/sejal-jain-25029817b",
-    role: "Chef d'équipe diffusion"
+    role: "Chef d'équipe diffusion",
   },
   {
     name: "Owen Brake",
     src: "./Headshots/owen.png",
     link: "https://OwenBrake.com",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Siyan Zhao",
     src: "./Headshots/siyan.png",
     link: "https://www.linkedin.com/in/siyanzhao/",
-    role: "Apprentissage Machine"
+    role: "Apprentissage Machine",
   },
   {
     name: "Robert Li",
     src: "./Headshots/robert.png",
     link: "https://www.linkedin.com/in/jianing-robert-li",
-    role: "Apprentissage Machine"
+    role: "Apprentissage Machine",
   },
   {
     name: "Robert Wu",
     src: "./Headshots/robertwu.png",
     link: "https://linkedin.com/in/wu-robert",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Rassam Yazdi",
     src: "./Headshots/rassam.png",
     link: "https://www.linkedin.com/in/rassam-yazdi-3a256319a/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Maanav Dalal",
     src: "./Headshots/maanav.png",
     link: "https://www.linkedin.com/in/maanavdalal/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Eric Zhai",
     src: "",
     link: "https://www.linkedin.com/in/eric-zhai/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Ivan Nesterovic",
     src: "./Headshots/ivan.png",
     link: "https://www.linkedin.com/in/ivan-nesterovic/",
-    role: "Développeur de logiciels"
+    role: "Développeur de logiciels",
   },
   {
     name: "Miraal Kabir",
     src: "./Headshots/miraal.png",
     link: "https://www.linkedin.com/in/miraalkabir/",
-    role: "UI/UX"
-  }
+    role: "UI/UX",
+  },
 ];
 
 export default volunteers;
